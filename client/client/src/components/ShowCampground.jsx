569bad5e9cee93ab1d18e062383f146bdee5e730
vckelly <vckelly@gmail.com>
2021-03-24 10:50:51 -0600

    feat: added spinner while loading data in ShowCampground component

--- client/client/src/components/ShowCampground.jsx
+++ client/client/src/components/ShowCampground.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Carousel from 'react-bootstrap/Carousel';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Spinner from 'react-bootstrap/Spinner';
 
 
 // const getCampgrounds = async () => {
@@ -18,60 +19,66 @@ import ListGroup from 'react-bootstrap/ListGroup';
 function ShowCampground() {
 
   let { id } = useParams();
-  const [campgroundState, setCampgroundState] = useState(null)
+  const [campgroundState, setCampgroundState] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData () {
       try {
         const response = await fetch(`http://localhost:5000/campgrounds/${id}`);
         const json = await response.json();
-        console.log(json);
         setCampgroundState(json);
-
+        setLoading(true);        
+        //console.log(campgroundState);
       } catch (error) {}
     }
-    if (campgroundState.length === 0 ) { 
-      fetchData() 
-    }
+    // if (campgroundState.length === 0 ) { 
+    //   fetchData() 
+    // }
+    
+    fetchData();
   }, [campgroundState]);
 
-  console.log(id, campgroundState);
-
+  //console.log(campgroundState);
   return (
     <div className="row">
-      <div className="col-6">
-        <h1>ShowCampground!</h1>
-        { campgroundState.length > 0 ? 
-          <Carousel>
-            { campgroundState.images.map((img) => {
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src={img.url}
-                />
-              </Carousel.Item>
-            })}
-          </Carousel>
-          <Card className="mb-3">
-            <Card.Body>
-              <Card.Title>{campgroundState.title}</Card.Title>  
-              <Card.Text>{campgroundState.description}</Card.Text>
-            </Card.Body>
-            <ListGroup variant="flush">
-              <ListGroup.Item className="text-muted">{campgroundState.location}</ListGroup.Item>
-              <ListGroup.Item>{"Submitted by " + campgroundState.author.username}</ListGroup.Item>
-              <ListGroup.Item>{"$" + campgroundState.price + "/night"}</ListGroup.Item>
-            </ListGroup>
-            <div className="col-6 text-muted">
-              2 days ago
-            </div>
-          </Card>
-          
-          : (<h1>Loading...</h1>)
+    <h1>ShowCampground!</h1>
+        { loading ? (
+          <div className="col-6">
+            { campgroundState.images[0] ?
+              (<Carousel>
+                { campgroundState.images.forEach((img, i) => {
+                    <Carousel.Item >
+                      <img
+                        className="d-block w-100"
+                        src={img.url}
+                        alt='Slide Here'
+                      />
+                    </Carousel.Item>
+                  }
+                )}                
+              </Carousel>) 
+              : ('')
+            }
+            <Card className="mb-3">
+              <Card.Body>
+                <Card.Title>{campgroundState.title}</Card.Title>  
+                <Card.Text>{campgroundState.description}</Card.Text>
+              </Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item className="text-muted">{campgroundState.location}</ListGroup.Item>
+                <ListGroup.Item>{"Submitted by " + campgroundState.author.username}</ListGroup.Item>
+                <ListGroup.Item>{"$" + campgroundState.price + "/night"}</ListGroup.Item>
+              </ListGroup>
+              <div className="col-6 text-muted">
+                2 days ago
+              </div>
+            </Card>
+          </div>) : (<Spinner animation="border" />)
         }
       </div>
-    </div>
   )
 }
 
 export default ShowCampground;
+
