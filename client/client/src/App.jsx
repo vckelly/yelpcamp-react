5f5b80ad4fbffdd05b105c583da2ea4293d6f095
vckelly <vckelly@gmail.com>
2021-03-25 15:48:56 -0600

    feat: added navbar component

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import Home from './components/Home.jsx';
 import Campgrounds from './components/Campgrounds.jsx';
 import ShowCampground from './components/ShowCampground.jsx';
+import CustomNav from './components/CustomNav.jsx';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
-import Axios from 'axios';
 
 function App() {
   return (
     <div className="App">
+      <CustomNav></CustomNav>
       <Switch>
         <Route path="/campgrounds/:id">
           <ShowCampground />
---
+++ client/client/src/components/CustomNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+export default function CustomNav() {
+    return (
+        <Navbar bg="dark" expand="lg" variant="lg" sticky="top">
+            <Container className="fluid">
+                <Navbar.Brand href="/home">ReactCamp</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/campgrounds">Campgrounds</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+};
