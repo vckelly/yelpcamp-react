c5a57b7cf119790f96bdbe5d5182d9c1e839f9e8
vckelly <vckelly@gmail.com>
2021-03-29 14:44:45 -0600

    feat: added redirect logic from Registration component

--- client/client/src/components/Register.jsx
+++ client/client/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from 'react'
+import { Redirect } from "react-router-dom"; 
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
@@ -8,10 +9,8 @@ import Form from 'react-bootstrap/Form'
 
 export default function Register() {
 
-    const [hasButtonBeenClicked, setButtonBeenClicked] = useState(false);
-    const [userState, setuserState] = useState(null);
+    const [redirect, setRedirect] = useState(false);
 
-    //TODO: Add axios post to register api
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(e.nativeEvent.target[0].value);
@@ -22,7 +21,6 @@ export default function Register() {
             password: e.nativeEvent.target[2].value
         };
 
-        console.log("DATA", data);
         fetch('http://localhost:5000/register', {
             method: 'POST',
             headers: {
@@ -31,46 +29,53 @@ export default function Register() {
             },
             body: JSON.stringify(data)
         }).then((res) => {
-            console.log("response", res)
+            //console.log("response", res);
+            if (res.status === 200) {
+                setRedirect(true);
+            }
         })
     };
-    // useEffect(() => {
-
-    // });
-
 
     return (
-        <Container>
-            <div className="row">
-                <div className="col-md-6 offset-md-3 col-xl-4 offset-xl-4">
-                    <Card variant="shadow">
-                        <Image src="https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
-                               alt="" className="card-img-top"/>
-                        <Card.Body>
-                            <Card.Title>Register</Card.Title>
-                            <Form onSubmit={handleSubmit}>
-                                <Form.Group controlId="email" >
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" name="email"  placeholder="Enter email" />
-                                    <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                    </Form.Text>
-                                </Form.Group>
-                                <Form.Group controlId="username">
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control type="username" name="username" placeholder="Enter username" />
-                                </Form.Group>
+        <div>
+        { redirect ? (
+            <Redirect 
+                to={{ pathname: "/campgrounds"}}
+            />
+        ) : (    
+            <Container>
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 col-xl-4 offset-xl-4">
+                        <Card variant="shadow">
+                            <Image src="https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80"
+                                alt="" className="card-img-top"/>
+                            <Card.Body>
+                                <Card.Title>Register</Card.Title>
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Group controlId="email" >
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" name="email"  placeholder="Enter email" />
+                                        <Form.Text className="text-muted">
+                                        We'll never share your email with anyone else.
+                                        </Form.Text>
+                                    </Form.Group>
+                                    <Form.Group controlId="username">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control type="username" name="username" placeholder="Enter username" />
+                                    </Form.Group>
 
-                                <Form.Group controlId="password">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" name="password" placeholder="Password" />
-                                </Form.Group>
-                                <Button variant="success" type="submit">Register</Button>{' '}
-                            </Form>
-                        </Card.Body>
-                    </Card>
+                                    <Form.Group controlId="password">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" name="password" placeholder="Password" />
+                                    </Form.Group>
+                                    <Button variant="success" type="submit">Register</Button>{' '}
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+            )}
+        </div>
     )
 };
